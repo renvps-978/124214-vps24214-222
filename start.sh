38c75c394f55dd9148fc9a920d5382f631c685a3
renvps-978 <YukiSultaire58@hotmail.com>
2025-10-05 08:29:43 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@ echo "🔹 Starting Cloudflare Tunnel..."
 # Chạy trực tiếp để log hiện ra stdout
 cloudflared tunnel --url http://localhost:4200 --no-autoupdate --metrics localhost:0 2>&1 | tee /root/tunnel.log | awk '/trycloudflare.com/ {print "🌍 Tunnel URL: "$NF; fflush()}'
 
-# Giữ container sống mãi
+
 tail -f /dev/null
